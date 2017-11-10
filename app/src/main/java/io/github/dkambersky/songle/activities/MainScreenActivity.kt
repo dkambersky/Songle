c1c4d3efbbd00543568a7d12a14123350df88878
David Kambersky <david.kambersky@gmail.com>
2017-11-10 16:59:23 +0000

    Removing data/wifi distinction for now

--- app/src/main/java/io/github/dkambersky/songle/activities/MainScreenActivity.kt
+++ app/src/main/java/io/github/dkambersky/songle/activities/MainScreenActivity.kt
@@ -15,19 +15,14 @@ import kotlinx.android.synthetic.main.activity_main_screen.*
 
 
 class MainScreenActivity : BaseActivity() {
-    private var receiver = NetworkReceiver()
+
     private var context: SongleContext? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
 
-
-        /* Register network receiver */
-        val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
-//        println("Registering receiver!")
-//        registerReceiver(receiver,filter)
-
+        
         /* Initialize context */
         context = SongleContext(mutableListOf(), mutableListOf(), applicationContext)
 
@@ -57,24 +52,6 @@ class MainScreenActivity : BaseActivity() {
     }
 
 
-    // TODO preferences (replace networkPref)!
-    private inner class NetworkReceiver : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            val connMgr =
-                    context.getSystemService(Context.CONNECTIVITY_SERVICE)
-                            as ConnectivityManager
-            val networkInfo = connMgr.activeNetworkInfo
-            if (/*networkPref == wifi && */
-            networkInfo?.type == ConnectivityManager.TYPE_WIFI) {
-                // Wi-Fi is connected, so use Wi-Fi
-            } else if (/*networkPref == any && */networkInfo != null) {
-                // Have a network connection and permission, so use data
-            } else {
-                // No Wi-Fi and no permission, or no network connection
-            }
-        }
-
-    }
 
 
 }
