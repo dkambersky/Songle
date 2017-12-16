2da949063d15ee20fb8e1dbec65ce85b44cb423b
David Kambersky <david.kambersky@gmail.com>
2017-12-16 01:01:44 +0000

    About page done

--- app/src/main/java/io/github/dkambersky/songle/activities/AboutActivity.kt
+++ app/src/main/java/io/github/dkambersky/songle/activities/AboutActivity.kt
@@ -1,13 +1,17 @@
 package io.github.dkambersky.songle.activities
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import io.github.dkambersky.songle.R
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        textView2.movementMethod = ScrollingMovementMethod()
     }
+
 
 }
